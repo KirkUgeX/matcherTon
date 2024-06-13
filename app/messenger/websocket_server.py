958264daa7ce7023563b7d10a0477cb97b6dc2c6
nn_workaet <nnworkaet@gmail.com>
2024-06-13 16:25:56 +0300

    ws update

--- app/messenger/websocket_server.py
+++ app/messenger/websocket_server.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+
 import psycopg2
 import websockets
 import json
@@ -8,12 +10,12 @@ from fastapi import HTTPException
 from app.databases.db import Database
 from app.core.security import encrypter, password
 from app.utils import uf
-
+from websockets import WebSocketServerProtocol
 
 connections = {}
 
 
-async def chat_server(websocket, path):
+async def chat_server(websocket: WebSocketServerProtocol, path: str):
     # Получение user_uuid из пути
     user_uuid = path.strip('/')
     connections[user_uuid] = websocket
@@ -38,10 +40,10 @@ async def chat_server(websocket, path):
                 message_data = json.loads(message)
                 print("Received message:", message_data)
 
-                user_uuid_from_db = await db.get_user_uuid_by_id(message_data["message"]["from_user"])
+                user_uuid_from_db = await db.get_user_uuid_by_id(message_data["from_user"])
 
-                check_user_in_chat = await db_m.check_user_in_chat(user_id=message_data["message"]["from_user"],
-                                                                   chat_id=message_data["message"]["chat_id"])
+                check_user_in_chat = await db_m.check_user_in_chat(user_id=message_data["from_user"],
+                                                                   chat_id=message_data["chat_id"])
 
                 if not check_user_in_chat:
                     error_message = json.dumps({"error": "User not found in chat"})
@@ -56,13 +58,13 @@ async def chat_server(websocket, path):
                 if message_data["event"] == "send_message":
                     new_message = {
                         "event": "new_message",
-                        "chat_id": message_data["message"]["chat_id"],
-                        "from_user": message_data["message"]["from_user"],
-                        "content": message_data["message"]["content"],
-                        "created_at": message_data["message"]["created_at"]
+                        "chat_id": message_data["chat_id"],
+                        "from_user": message_data["from_user"],
+                        "content": message_data["content"],
+                        "created_at": message_data["created_at"]
                     }
-                    chat_users_id = await db_m.get_other_user_in_chat(message_data["message"]["chat_id"],
-                                                                      message_data["message"]["from_user"])
+                    chat_users_id = await db_m.get_other_user_in_chat(message_data["chat_id"],
+                                                                      message_data["from_user"])
                     receiver_uuid = str(await db.get_user_uuid_by_id(chat_users_id))
 
                     if receiver_uuid in connections:
@@ -76,18 +78,19 @@ async def chat_server(websocket, path):
                     encrypt_res = await encrypter(password=password, data=message_data["content"])
                     encrypt_message, key = encrypt_res["encrypted_data"], encrypt_res["key"]
                     message_id = await db_m.add_message(
-                        chat_id=message_data["message"]["chat_id"],
-                        from_user=message_data["message"]["from_user"],
+                        chat_id=message_data["chat_id"],
+                        from_user=message_data["from_user"],
                         content=encrypt_message,
                         reply_to=0,
                         attachment_id=None,
-                        created_at=message_data["message"]["created_at"],
+                        created_at=message_data["created_at"],
                         key=key
                     )
-                    chat_users = await db_m.get_chat_users(chat_id=message_data["message"]["chat_id"])
-                    messages_num = await db_m.count_messages_in_chat(chat_id=message_data["message"]["chat_id"])
-                    num_messages_by_one = await db_m.count_messages_by_user_in_chat(user_id=message_data["message"]["from_user"],
-                                                                                    chat_id=message_data["message"]["chat_id"])
+                    chat_users = await db_m.get_chat_users(chat_id=message_data["chat_id"])
+                    messages_num = await db_m.count_messages_in_chat(chat_id=message_data["chat_id"])
+                    num_messages_by_one = await db_m.count_messages_by_user_in_chat(
+                        user_id=message_data["from_user"],
+                        chat_id=message_data["chat_id"])
 
                     if not isinstance(num_messages_by_one, int) or not isinstance(messages_num, int):
                         raise HTTPException(status_code=502, detail=f"Error counting messages by user in chat")
@@ -115,9 +118,9 @@ async def chat_server(websocket, path):
     finally:
         del connections[user_uuid]
 
+load_dotenv()
+start_server = websockets.serve(chat_server, os.getenv("WS_SER"), os.getenv("WS_PORT"))
 
-start_server = websockets.serve(chat_server, '127.0.0.1', 666)  # 38.242.233.161
-print(f"Websocket server started and listening on ws://38.242.233.161:8765")
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()

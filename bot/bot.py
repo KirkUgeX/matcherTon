c6fcd40d6cb04245789e516c3e3280c64c1ddc33
nn_workaet <nnworkaet@gmail.com>
2024-06-25 17:45:09 +0300

    bot

---
+++ bot/bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+from config_reader import config
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from messages import welcome_message
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config.bot_token.get_secret_value())
+
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer(welcome_message)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ bot/messages.py
@@ -0,0 +1,11 @@
+welcome_message = """
+🔗 **Welcome to Matcher!** 🌐
+
+Welcome to Matcher, your ultimate Web3 networking app! 🚀
+
+Here, you can swipe to like or dislike profiles based on both their detailed information and on-chain hard-skills verification. Say goodbye to networking fears and hello to genuine connections! 🌟
+
+Whether you're seeking new partners, friends, or collaborators in the Web3 space, Matcher provides a safe and authentic environment to connect with other builders.
+
+Start swiping, chatting, and meeting your perfect match today. Happy networking! ✨
+"""
